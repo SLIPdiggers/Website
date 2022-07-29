06296c9a1829a17a15dcc0901ec1b440f28cfed5
Ben Gladders <benglanders@hotmail.com>
2022-07-29 18:40:06 +0100

    Added intro fuzz..

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
 	"The typeof operator returns the type of the argument." https://javascript.info/types
 	
 */
-let fuzzy_timer = 999;
+let fuzzy_timer = 300;
+let grey_scale = 0;
 
 let canvas = document.getElementById('video');
 let canvas_width = document.getElementsByTagName('html')[0].clientWidth;
@@ -31,7 +32,7 @@ function run_script(){		//use mocha for testing?
 	}
 }
 
-class AlphaSprite {
+class AlphabetSprite {
 	constructor(x_pos, y_pos) {
 		if (x_pos > -1 && x_pos < canvas_width + 1) this.x_pos = x_pos;
 		else this.x_pos = -1;
@@ -49,18 +50,21 @@ function draw_static(ctx) {
 	//fill background black - may be better to transparent!!
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, canvas_width, canvas_height);
-		
-	for (let i = 0; i < (canvas_width>canvas_height?canvas_width:canvas_height)*10; i++) {
 	
 	fuzzy_timer--;
+	
+	for (let i = 0; i < (canvas_width>canvas_height?canvas_width:canvas_height)*10; i++) {
+	
+	
 	if (fuzzy_timer > 255){
 	//	let greyScale = Math.floor(Math.random()*255);
+		grey_scale = 255;
 	}else{
-		
+		grey_scale = fuzzy_timer;
 	}
 	//ctx.fillStyle = `rgba(${greyScale}, ${greyScale}, ${greyScale}, 255)`;
 	
-	ctx.fillStyle = `rgba(${greyScale}, ${greyScale}, ${greyScale}, 255)`;
+	ctx.fillStyle = `rgba(${grey_scale}, ${grey_scale}, ${grey_scale}, 255)`;
 	ctx.fillRect(Math.random() * canvas_width, Math.random() * canvas_height, 2, 2);
 	}
 }
@@ -72,7 +76,7 @@ function draw_overlay() {
 	}
 	
 	let ctx = canvas.getContext('2d');
-	if fuzzy_timer > 0 {
+	if (fuzzy_timer > 0){
 		draw_static(ctx);
 	}
 
@@ -98,65 +102,11 @@ function draw_overlay() {
 		ctx.lineWidth = 3;
 		ctx.stroke();
 
-		
-			if (r_ascend == true) {
-				//alert('r_ascend true and r=' + r + ', g=' + g + ', b=' + b);
-				r+=4;
-				b-=4;
-				if (r > 254) {
-					r_ascend = false;
-					g_ascend = true;
-				}
-			}
-			if (g_ascend == true) {
-				//alert('g_ascend true and r=' + r + ', g=' + g + ', b=' + b);
-				g+=4;
-				r-=4;
-				if (g > 254) {
-					g_ascend = false;
-					b_ascend = true;
-				}
-			}
-			if (b_ascend == true) {
-				//alert('b_ascend true and r=' + r + ', g=' + g + ', b=' + b);
-				b+=4;
-				g-=4;
-				if (b > 254) {
-					b_ascend = false;
-					r_ascend = true;
-				}
-			}
-
-
-			ctx.font = "48px Impact";
-			ctx.fillStyle = `rgb(${r},${g},${b})`;
-			ctx.textAlign = "center";
-			ctx.fillText("Under Construction", canvas_width / 2, canvas_height / 2);
-
-			ctx.font = "10px Impact";
-			ctx.fillStyle = `rgb(${r},${g},${b})`;
-			ctx.textAlign = "rleft";
-			ctx.fillText(`red: ${r}`, (canvas_width / 30) * 28, (canvas_height / 30) * 26);
-			ctx.fillText(`green: ${g}`, (canvas_width / 30) * 28, (canvas_height / 30) * 27);
-			ctx.fillText(`blue: ${b}`, (canvas_width / 30) * 28, (canvas_height / 30) * 28);
-
 			ctx.beginPath();
 			let bar_right = (canvas_width / 100) * 95;
 			let bar_top = (canvas_height / 100) * 88;
 			ctx.strokeStyle = 'white';
 			ctx.lineWidth = 1;
-
-			ctx.moveTo(bar_right, bar_top); //r bar
-			ctx.lineTo(bar_right - (r / (canvas_width/200)), bar_top);
-			ctx.closePath();
-
-			ctx.moveTo(bar_right, (bar_top / 88) * 91.5); //g bar
-			ctx.lineTo(bar_right - (g / (canvas_width / 200)), (bar_top / 88) * 91.5);
-			ctx.closePath();
-
-			ctx.moveTo(bar_right, (bar_top / 88) * 94.5); //b bar
-			ctx.lineTo(bar_right - (b / (canvas_width / 200)), (bar_top / 88) * 94.5);
-			ctx.closePath();
 
 			ctx.stroke();
 		
